5fae8869ac9f492f909c50cd9625204fb99dedd3
reis-marta <163763026+reis-marta@users.noreply.github.com>
2024-05-04 15:10:32 +0100

    icon image data

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ function updateWeather(response) {
   let speedElement = document.querySelector("#speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let icon = document.querySelector("#icon");
 
+  icon.innerHTML = `<img src="${response.data.condition.icon_url}" class="wearher-data-icon" />`;
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
