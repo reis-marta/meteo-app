6a90a186248a70d245544af01be711260e7eaa6a
reis-marta <163763026+reis-marta@users.noreply.github.com>
2024-05-04 18:52:21 +0100

    js data

--- src/index.js
+++ src/index.js
@@ -51,5 +51,33 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+        <div class="flex-item">
+    <div class="weather-forecast-day">${day}</div>
+    <img
+      src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/few-clouds-day.png"
+      alt="icon"
+      width="50px"
+    />
+    <div class="weather-forecast-temperatures"><b>10º</b> 20º</div>
+  </div>
+  `;
+  });
+
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Lisbon");
+displayForecast();
